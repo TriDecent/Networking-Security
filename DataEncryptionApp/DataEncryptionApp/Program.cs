d5204c4794adb4af261d9482035b2b16807d9f12
TriDecent <116976929+TriDecent@users.noreply.github.com>
2024-09-27 17:08:19 +0700

    add once method in IDataEncryptionRepository and update the old one

--- DataEncryptionApp/DataEncryptionApp/Program.cs
+++ DataEncryptionApp/DataEncryptionApp/Program.cs
@@ -186,7 +186,8 @@ public interface IDataEncryptionRepository
 {
   IEnumerable<string> ReadFromFile(string filePath);
   void EncryptToFile(string filePath, IEnumerable<string> plainTextBlocks, string key);
-  void DecryptToFile(string filePath, IEnumerable<string> cipherTextBlocks);
+  void DecryptToFile(string filePath, IEnumerable<string> cipherTextBlocks, string key);
+  void CrackingDecryptToFile(string filePath, IEnumerable<string> cipherTextBlocks);
 }
 
 public class DataEncryptionRepository(
