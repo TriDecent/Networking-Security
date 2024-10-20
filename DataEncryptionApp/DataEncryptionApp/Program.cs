860ee85b4b07706419d6724911f0aabb56a5ff23
TriDecent <116976929+TriDecent@users.noreply.github.com>
2024-10-20 20:57:08 +0700

    add encryption method selection in DataEncryptionApp

--- DataEncryptionApp/DataEncryptionApp/Program.cs
+++ DataEncryptionApp/DataEncryptionApp/Program.cs
@@ -1,6 +1,8 @@
 ﻿using DataEncryptionApp.App;
 using DataEncryptionApp.DataAccess;
+using DataEncryptionApp.DataEncryption;
 using DataEncryptionApp.DataEncryption.AffineCipher;
+using DataEncryptionApp.DataEncryption.AsymmetricCipher;
 using DataEncryptionApp.DataEncryption.PlayFair;
 using DataEncryptionApp.DataEncryption.ShiftCipher;
 using DataEncryptionApp.UI;
@@ -10,9 +12,26 @@ using DataEncryptionApp.UI;
 
 try
 {
-  var currentEncryption = new AffineEncryption(); // Alters here to use another encryption.
+  var uiHandler = new ConsoleUI();
+  uiHandler.DisplayMessage("Select encryption method:");
+  uiHandler.DisplayMessage("1. Affine Encryption");
+  uiHandler.DisplayMessage("2. PlayFair Encryption");
+  uiHandler.DisplayMessage("3. Caesar Encryption");
+  uiHandler.DisplayMessage("4. Vigenere Encryption");
+  uiHandler.DisplayMessage("5: RSA Encryption");
+  uiHandler.DisplayMessage("Note: Caesar Encryption will be used for any incorrect input.");
+  uiHandler.DisplayMessageWithoutNewLine("Enter choice (1/2/3/4): ");
+  var choice = Console.ReadLine();
 
-  // var playFairEncryption = new PlayFairEncryption("Harry Potter");
+  ICrackingDataEncryption currentEncryption = choice switch
+  {
+    "1" => new AffineEncryption(),
+    "2" => new PlayFairEncryption(),
+    "3" => new CaesarEncryption(),
+    "4" => new VigenereEncryption(),
+    "5" => new RSAEncryption(),
+    _ => new CaesarEncryption()
+  };
 
   var dataEncryption = new DataEncryptionApplication(
     currentEncryption, // default 
@@ -22,7 +41,7 @@ try
       currentEncryption, // cracking
       new TextualStringsRepository()
     ),
-    new DataEncryptionUI(new ConsoleUI())
+    new DataEncryptionUI(uiHandler)
   );
 
   dataEncryption.Run();
--- DataEncryptionApp/DataEncryptionApp/UI/ConsoleUI.cs
+++ DataEncryptionApp/DataEncryptionApp/UI/ConsoleUI.cs
@@ -5,6 +5,9 @@ public class ConsoleUI : IUIHandler
   public void DisplayMessage(string message)
     => Console.WriteLine(message);
 
+  public void DisplayMessageWithoutNewLine(string message)
+    => Console.Write(message);
+
   public string? GetFromUser()
     => Console.ReadLine();
 }
