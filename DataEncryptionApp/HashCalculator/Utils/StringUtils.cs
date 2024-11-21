c21394a69bdb9ba180ecf614ef045831644b47e6
TriDecent <116976929+TriDecent@users.noreply.github.com>
2024-11-22 03:44:55 +0700

    enhance input validation with two new checks in StringUtils

---
+++ DataEncryptionApp/HashCalculator/Utils/StringUtils.cs
@@ -0,0 +1,25 @@
+namespace HashCalculator;
+
+public static class StringUtils
+{
+  public static byte[] HexToBytes(this string hex)
+  {
+    if (string.IsNullOrEmpty(hex))
+    {
+      throw new InvalidOperationException("Hex string cannot be null or empty");
+    }
+
+    if (hex.Length % 2 != 0)
+    {
+      throw new InvalidOperationException("Hex string must have an even length");
+    }
+
+    byte[] bytes = new byte[hex.Length / 2];
+    for (int i = 0; i < hex.Length; i += 2)
+    {
+      bytes[i / 2] = Convert.ToByte(hex.Substring(i, 2), 16);
+    }
+
+    return bytes;
+  }
+}
