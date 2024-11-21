bf9dbeb360309831e1b1b73e9ed643df264ddb1f
TriDecent <116976929+TriDecent@users.noreply.github.com>
2024-11-22 02:27:32 +0700

    add IHashGenerator interface and MD5HashGenerator implementation for hash generation

--- DataEncryptionApp/HashCalculator/HashCalculatorForm.cs
+++ DataEncryptionApp/HashCalculator/HashCalculatorForm.cs
@@ -12,6 +12,8 @@ public partial class HashCalculatorForm : Form
 
   private DataFormat _dataFormat;
 
+  private IHashGenerator _hashGenerator;
+
   public HashCalculatorForm()
   {
     InitializeComponent();
@@ -26,6 +28,8 @@ public partial class HashCalculatorForm : Form
     _cbDataFormat.DataSource = Enum.GetValues(typeof(DataFormat));
 
     _cbDataFormat.SelectedValueChanged += OnDataFormatChanged;
+
+    _txtData.TextChanged += OnDataChanged;
   }
 
   private void OnDataFormatChanged(object? sender, EventArgs e)
@@ -43,5 +47,11 @@ public partial class HashCalculatorForm : Form
         _txtData.Text = openFileDialog.FileName;
       }
     }
+  }
+
+  private void OnDataChanged(object? sender, EventArgs e)
+  {
+    _hashGenerator = new MD5HashGenerator();
+    _txtMD5Hash.Text = _hashGenerator.GenerateHash(_txtData.Text, _dataFormat);
   }
 }
---
+++ DataEncryptionApp/HashCalculator/IHashGenerator.cs
@@ -0,0 +1,67 @@
+﻿using System.CodeDom.Compiler;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace HashCalculator;
+
+internal interface IHashGenerator
+{
+  string GenerateHash(string data, DataFormat dataFormat);
+}
+
+public class MD5HashGenerator : IHashGenerator
+{
+  public string GenerateHash(string data, DataFormat dataFormat)
+  => dataFormat switch
+  {
+    DataFormat.Text => GenerateHashFromText(data),
+    DataFormat.Hex => GenerateHashFromHex(data),
+    DataFormat.File => GenerateHashFromFile(data),
+    _ => throw new NotSupportedException($"Data format {dataFormat} is not supported."),
+  };
+
+  private static string GenerateHashFromText(string data)
+  {
+    using MD5 md5 = MD5.Create();
+
+    var inputBytes = Encoding.UTF8.GetBytes(data);
+    var hashBytes = MD5.HashData(inputBytes);
+    var stringBuilder = new StringBuilder();
+    for (int i = 0; i < hashBytes.Length; i++)
+    {
+      stringBuilder.Append(hashBytes[i].ToString("X2"));
+    }
+
+    return stringBuilder.ToString();
+  }
+
+  private static string GenerateHashFromHex(string hexString)
+  {
+    using MD5 md5 = MD5.Create();
+
+    var inputBytes = hexString.HexToBytes();
+    var hashBytes = MD5.HashData(inputBytes);
+    var stringBuilder = new StringBuilder();
+    for (int i = 0; i < hashBytes.Length; i++)
+    {
+      stringBuilder.Append(hashBytes[i].ToString("X2"));
+    }
+
+    return stringBuilder.ToString();
+  }
+
+  private static string GenerateHashFromFile(string filePath)
+  {
+    using MD5 md5 = MD5.Create();
+
+    var inputBytes = File.ReadAllBytes(filePath);
+    var hashBytes = MD5.HashData(inputBytes);
+    var stringBuilder = new StringBuilder();
+    for (int i = 0; i < hashBytes.Length; i++)
+    {
+      stringBuilder.Append(hashBytes[i].ToString("X2"));
+    }
+
+    return stringBuilder.ToString();
+  }
+}
