efd28db4ab6b0e21c27028f09bb59e9baca6ddde
TriDecent <116976929+TriDecent@users.noreply.github.com>
2024-11-22 14:55:50 +0700

    enhance SHA256HashGenerator: add hash generation for text, hex, and file formats; remove SHA2HashGenerator

---
+++ DataEncryptionApp/HashCalculator/HashGenerators/SHA256HashGenerator.cs
@@ -0,0 +1,38 @@
+using HashCalculator.Enums;
+using HashCalculator.Utils;
+using System.Security.Cryptography;
+
+namespace HashCalculator.HashGenerators;
+
+internal class SHA256HashGenerator : IHashGenerator
+{
+  public string GenerateHash(string data, DataFormat dataFormat)
+    => dataFormat switch
+    {
+      DataFormat.Text => GenerateHashFromText(data),
+      DataFormat.Hex => GenerateHashFromHex(data),
+      DataFormat.File => GenerateHashFromFile(data),
+      _ => throw new NotSupportedException($"Data format {dataFormat} is not supported."),
+    };
+
+  private static string GenerateHashFromText(string data)
+  {
+    var bytes = System.Text.Encoding.UTF8.GetBytes(data);
+    var hash = SHA256.HashData(bytes);
+    return BitConverter.ToString(hash).Replace("-", "").ToLowerInvariant();
+  }
+
+  private static string GenerateHashFromHex(string data)
+  {
+    var bytes = data.HexToBytes();
+    var hash = SHA256.HashData(bytes);
+    return BitConverter.ToString(hash).Replace("-", "").ToLowerInvariant();
+  }
+
+  private static string GenerateHashFromFile(string data)
+  {
+    var bytes = File.ReadAllBytes(data);
+    var hash = SHA256.HashData(bytes);
+    return BitConverter.ToString(hash).Replace("-", "").ToLowerInvariant();
+  }
+}
--- DataEncryptionApp/HashCalculator/HashGenerators/SHA2HashGenerator.cs
+++
@@ -1,9 +0,0 @@
-namespace HashCalculator;
-
-internal class SHA2HashGenerator : IHashGenerator
-{
-  public string GenerateHash(string data, DataFormat dataFormat)
-  {
-    throw new NotImplementedException();
-  }
-}
