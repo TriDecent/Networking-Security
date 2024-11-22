a573521e0da9d89543ac61290b9c63ccb23eae2c
TriDecent <116976929+TriDecent@users.noreply.github.com>
2024-11-22 16:03:42 +0700

    enhance HashCalculatorForm: enhance HashCalculatorForm: using SHA3-512 in the form

--- DataEncryptionApp/HashCalculator/HashCalculatorForm.cs
+++ DataEncryptionApp/HashCalculator/HashCalculatorForm.cs
@@ -13,10 +13,12 @@ public partial class HashCalculatorForm : Form
   private readonly TextBox _txtMD5Hash;
   private readonly TextBox _txtSHA1Hash;
   private readonly TextBox _txtSHA2Hash;
+  private readonly TextBox _txtSHA3_512Hash;
 
   private readonly CheckBox _cbUseMD5;
   private readonly CheckBox _cbUseSHA1;
   private readonly CheckBox _cbUseSHA256;
+  private readonly CheckBox _cbUseSHA3_512;
 
   private DataFormat _dataFormat;
 
@@ -29,9 +31,12 @@ public partial class HashCalculatorForm : Form
     _txtMD5Hash = txtMD5Hash;
     _txtSHA1Hash = txtSHA1Hash;
     _txtSHA2Hash = txtSHA256Hash;
+    _txtSHA3_512Hash = txtSHA3_512Hash;
     _cbUseMD5 = cbMD5;
     _cbUseSHA1 = cbSHA1;
     _cbUseSHA256 = cbSHA256;
+    _cbUseSHA3_512 = cbSHA3_512;
+
     _cbDataFormat = cbDataFormat;
 
     _cbDataFormat.DataSource = Enum.GetValues(typeof(DataFormat));
@@ -42,10 +47,12 @@ public partial class HashCalculatorForm : Form
     _cbUseMD5.CheckedChanged += OnDataChanged;
     _cbUseSHA1.CheckedChanged += OnDataChanged;
     _cbUseSHA256.CheckedChanged += OnDataChanged;
+    _cbUseSHA3_512.CheckedChanged += OnDataChanged;
 
     _cbUseMD5.CheckedChanged += OnCheckedChanged;
     _cbUseSHA1.CheckedChanged += OnCheckedChanged;
     _cbUseSHA256.CheckedChanged += OnCheckedChanged;
+    _cbUseSHA3_512.CheckedChanged += OnCheckedChanged;
   }
 
   private void OnDataFormatChanged(object? sender, EventArgs e)
@@ -78,6 +85,7 @@ public partial class HashCalculatorForm : Form
       if (_cbUseMD5.Checked) _txtMD5Hash.Text = GenerateHash(new MD5HashGenerator());
       if (_cbUseSHA1.Checked) _txtSHA1Hash.Text = GenerateHash(new SHA1HashGenerator());
       if (_cbUseSHA256.Checked) _txtSHA2Hash.Text = GenerateHash(new SHA256HashGenerator());
+      if (_cbUseSHA3_512.Checked) _txtSHA3_512Hash.Text = GenerateHash(new SHA3_512HashGenerator());
     }
     catch (Exception ex) when (ex is InvalidOperationException || ex is FormatException)
     {
@@ -90,6 +98,7 @@ public partial class HashCalculatorForm : Form
     _txtMD5Hash.Text = string.Empty;
     _txtSHA1Hash.Text = string.Empty;
     _txtSHA2Hash.Text = string.Empty;
+    _txtSHA3_512Hash.Text = string.Empty;
   }
 
   private string GenerateHash(IHashGenerator hashGenerator)
@@ -100,5 +109,6 @@ public partial class HashCalculatorForm : Form
     _txtMD5Hash.Text = _cbUseMD5.Checked ? _txtMD5Hash.Text : string.Empty;
     _txtSHA1Hash.Text = _cbUseSHA1.Checked ? _txtSHA1Hash.Text : string.Empty;
     _txtSHA2Hash.Text = _cbUseSHA256.Checked ? _txtSHA2Hash.Text : string.Empty;
+    _txtSHA3_512Hash.Text = _cbUseSHA3_512.Checked ? _txtSHA3_512Hash.Text : string.Empty;
   }
 }
