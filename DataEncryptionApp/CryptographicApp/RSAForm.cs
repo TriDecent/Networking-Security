f7f7847e85fa9ecace8a8b4f0c5a3d0d4aed52f0
TriDecent <116976929+TriDecent@users.noreply.github.com>
2024-12-20 07:34:42 +0700

    refactor the structure of methods and implement hex encryption feature

--- DataEncryptionApp/CryptographicApp/RSAForm.cs
+++ DataEncryptionApp/CryptographicApp/RSAForm.cs
@@ -1,3 +1,4 @@
+using System.Diagnostics;
 using System.Security.Cryptography;
 using System.Text.RegularExpressions;
 
@@ -9,8 +10,14 @@ public partial class RSAForm : Form
   private readonly Button _btnEncrypt, _btnDecrypt;
   private readonly ComboBox _cbDataFormat, _cbPadding, _cbKeySize;
   private readonly TextBox _txtDataOrFilePath, _txtResult, _txtImportedKeyName;
+  private readonly Label _lblTimeTook;
+  private readonly ProgressBar _progressBar;
 
   private string _importedKeyFilePath = "";
+
+  private DataFormat _selectedDataFormat = DataFormat.Text;
+  private RSAEncryptionPadding _selectedPadding = RSAEncryptionPadding.Pkcs1;
+  private int _selectedKeySize = 1024;
 
   public RSAForm()
   {
@@ -27,6 +34,8 @@ public partial class RSAForm : Form
     _txtDataOrFilePath = txtDataOrFilePath;
     _txtResult = txtResult;
     _txtImportedKeyName = txtImportedKeyName;
+    _lblTimeTook = lblTimeTook;
+    _progressBar = progressBar;
 
     _cbDataFormat.DataSource = Enum.GetValues<DataFormat>();
     _cbPadding.DataSource = new[]
@@ -37,55 +46,117 @@ public partial class RSAForm : Form
     };
     _cbKeySize.DataSource = new[] { 1024, 2048, 3072, 4096 };
 
+    _cbDataFormat.SelectedValueChanged += (s, e) => OnSelectedDataFormatChanged();
+    _cbPadding.SelectedValueChanged += (s, e) => OnSelectedPaddingChanged();
+    _cbKeySize.SelectedValueChanged += (s, e) => OnSelectedKeySizeChanged();
+
     _btnGenerateKey.Click += (s, e) => OnGenerateKeyClicked();
     _btnImportKey.Click += (s, e) => OnImportKeyClicked();
     _btnEncrypt.Click += (s, e) => OnEncryptClicked();
   }
 
+  private void OnSelectedDataFormatChanged()
+  => _selectedDataFormat = (DataFormat)_cbDataFormat.SelectedItem!;
+  private void OnSelectedPaddingChanged()
+    => _selectedPadding = (RSAEncryptionPadding)_cbPadding.SelectedItem!;
+
+  private void OnSelectedKeySizeChanged()
+    => _selectedKeySize = (int)_cbKeySize.SelectedItem!;
+
   private void OnGenerateKeyClicked()
   {
-    var padding = (RSAEncryptionPadding)_cbPadding.SelectedItem!;
-    var keySize = (int)_cbKeySize.SelectedItem!;
+    var stopWatch = Stopwatch.StartNew();
 
-    var rsa = RSA.Create(keySize);
-    var rsaEncryption = new RSAEncryption(rsa, padding);
-    var (PublicKey, PrivateKey) = rsaEncryption.GenerateKey();
+    var rsa = RSA.Create(_selectedKeySize);
+    var rsaEncryption = new RSAEncryption(rsa, _selectedPadding);
+    var (publicKey, privateKey) = rsaEncryption.GenerateKey();
 
-    SaveKeyPairs(PublicKey, PrivateKey);
+    SaveKeyPairs(publicKey, privateKey);
+
+    stopWatch.Stop();
+    DisplayTimeTook(stopWatch.ElapsedMilliseconds);
+
+    ShowSuccessMessage("Keys have been successfully generated and saved in the KeyPairs folder.");
   }
 
   private void OnImportKeyClicked()
   {
-    var openDialog = new OpenFileDialog()
+    using var openDialog = new OpenFileDialog
     {
       Filter = "PEM files (*.pem)|*.pem|All files (*.*)|*.*"
     };
 
     if (openDialog.ShowDialog() == DialogResult.OK)
     {
-      var filePath = openDialog.FileName;
-      _importedKeyFilePath = filePath;
-
-      var fileName = filePath.Split("\\", StringSplitOptions.RemoveEmptyEntries).LastOrDefault();
-      _txtImportedKeyName.Text = fileName;
+      _importedKeyFilePath = openDialog.FileName;
+      _txtImportedKeyName.Text = Path.GetFileName(_importedKeyFilePath);
     }
   }
 
   private void OnEncryptClicked()
   {
-    if (string.IsNullOrEmpty(_txtDataOrFilePath.Text)) return;
-    
-    var dataFormat = (DataFormat)_cbDataFormat.SelectedItem!;
-    var padding = (RSAEncryptionPadding)_cbPadding.SelectedItem!;
-    var keySize = (int)_cbKeySize.SelectedItem!;
+    if (string.IsNullOrEmpty(_txtDataOrFilePath.Text) || string.IsNullOrEmpty(_txtImportedKeyName.Text))
+    {
+      ShowErrorMessage("Data or file path and imported key name cannot be empty.");
+      return;
+    }
 
-    var rsa = RSA.Create(keySize);
     var publicKeyPem = File.ReadAllText(_importedKeyFilePath);
+    var rsa = RSA.Create();
 
-    var rsaEncryption = new RSAEncryption(rsa, padding);
-    var encryptedData = rsaEncryption.Encrypt(_txtDataOrFilePath.Text, publicKeyPem, dataFormat);
+    var rsaEncryption = new RSAEncryption(rsa, _selectedPadding);
 
-    _txtResult.Text = encryptedData;
+    var stopWatch = Stopwatch.StartNew();
+    ToggleButton(_btnEncrypt);
+    try
+    {
+      stopWatch.Start();
+      if (_selectedDataFormat == DataFormat.File)
+      {
+        HandleFileEncryption(rsaEncryption, publicKeyPem, _txtDataOrFilePath.Text);
+        return;
+      }
+
+      HandleStringEncryption(rsaEncryption, publicKeyPem, _txtDataOrFilePath.Text);
+    }
+    catch (CryptographicException ex)
+    {
+      ShowErrorMessage(ex.Message);
+    }
+    finally
+    {
+      stopWatch.Stop();
+      DisplayTimeTook(stopWatch.ElapsedMilliseconds);
+      ToggleButton(_btnEncrypt);
+    }
+  }
+
+  private void HandleStringEncryption(RSAEncryption rsa, string publicKeyPem, string dataString)
+  {
+    if (_selectedDataFormat == DataFormat.Text)
+    {
+      HandleTextEncryption(rsa, publicKeyPem, dataString);
+      return;
+    }
+
+    if (!dataString.IsHexString())
+    {
+      ShowErrorMessage("The provided data is not a valid hexadecimal string.");
+      return;
+    }
+
+    HandleHexEncryption(rsa, publicKeyPem, dataString);
+  }
+
+  private void HandleTextEncryption(RSAEncryption rsa, string publicKeyPem, string text)
+    => _txtResult.Text = rsa.Encrypt(text, publicKeyPem, _selectedDataFormat);
+
+  private void HandleHexEncryption(RSAEncryption rsa, string publicKeyPem, string hex)
+    => _txtResult.Text = rsa.Encrypt(hex, publicKeyPem, _selectedDataFormat);
+
+  private void HandleFileEncryption(RSAEncryption rsa, string publicKeyPem, string text)
+  {
+    throw new NotImplementedException();
   }
 
 
@@ -94,27 +165,35 @@ public partial class RSAForm : Form
 
   private static void SaveKeyPairs(string publicKey, string privateKey)
   {
-    var baseDir = Path.GetDirectoryName(Application.ExecutablePath)!;
-    var directory = Path.Combine(baseDir, "KeyPairs");
+    var directory = Path.Combine(Path.GetDirectoryName(Application.ExecutablePath)!, "KeyPairs");
     Directory.CreateDirectory(directory);
 
-    var files = Directory.GetFiles(directory, "public_key*.pem");
-    int highestNumber = 0;
+    var newNumber = Directory.GetFiles(directory, "public_key*.pem")
+      .Select(file => KeyNumberPattern().Match(file))
+      .Where(match => match.Success && int.TryParse(match.Groups[1].Value, out _))
+      .Select(match => int.Parse(match.Groups[1].Value))
+      .DefaultIfEmpty(0)
+      .Max() + 1;
 
-    foreach (var file in files)
-    {
-      var match = KeyNumberPattern().Match(file);
-      if (match.Success && int.TryParse(match.Groups[1].Value, out int number))
-      {
-        highestNumber = Math.Max(highestNumber, number);
-      }
-    }
-
-    var newNumber = highestNumber + 1;
-    string publicKeyPath = Path.Combine(directory, $"public_key{newNumber}.pem");
-    string privateKeyPath = Path.Combine(directory, $"private_key{newNumber}.pem");
-
-    File.WriteAllText(publicKeyPath, publicKey);
-    File.WriteAllText(privateKeyPath, privateKey);
+    File.WriteAllText(Path.Combine(directory, $"public_key{newNumber}.pem"), publicKey);
+    File.WriteAllText(Path.Combine(directory, $"private_key{newNumber}.pem"), privateKey);
   }
+
+  private void DisplayTimeTook(long elapsedMilliseconds)
+    => _lblTimeTook.Text = $"Time took: {elapsedMilliseconds} ms";
+
+  private static void ShowSuccessMessage(string message) => MessageBox.Show(
+    message,
+    "Success",
+    MessageBoxButtons.OK,
+    MessageBoxIcon.Information);
+
+  private static void ShowErrorMessage(string message) => MessageBox.Show(
+    message,
+    "Error",
+    MessageBoxButtons.OK,
+    MessageBoxIcon.Error);
+
+  private static void ToggleButton(Button button)
+    => button.Enabled = !button.Enabled;
 }
