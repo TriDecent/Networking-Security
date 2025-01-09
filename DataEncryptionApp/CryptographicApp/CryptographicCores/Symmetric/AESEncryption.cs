07321b1c5381f1513346be9710c2da86f3f4ee28
TriDecent <116976929+TriDecent@users.noreply.github.com>
2025-01-09 22:57:47 +0700

    add AES encryption and decryption methods, including padding and key size configuration

--- DataEncryptionApp/CryptographicApp/CryptographicCores/Symmetric/AESEncryption.cs
+++ DataEncryptionApp/CryptographicApp/CryptographicCores/Symmetric/AESEncryption.cs
@@ -1,24 +1,57 @@
 using System.Security.Cryptography;
 using CryptographicApp.Models;
+using CryptographicApp.Utils;
 
 namespace CryptographicApp.CryptographicCores.Symmetric;
 
 public interface IAes
 {
+  void SetPadding(PaddingMode padding);
+  void SetKeySize(int keySize);
   AESKey GenerateKey();
+  Task EncryptFileAsync(string inputFilePath, string outputFilePath, AESKey aesKey);
+  Task DecryptFileAsync(string inputFilePath, string outputFilePath, AESKey aesKey);
 }
 
-public class AESEncryption(Aes aes, PaddingMode padding) : IAes
+public class AESEncryption(Aes aes) : IAes
 {
   private readonly Aes _aes = aes;
-  private readonly PaddingMode _padding = padding;
+
+  public void SetPadding(PaddingMode padding) => _aes.Padding = padding;
+
+  public void SetKeySize(int keySize) => _aes.KeySize = keySize;
 
   public AESKey GenerateKey()
   {
     _aes.GenerateKey();
     _aes.GenerateIV();
 
-    return new AESKey(
-      Convert.ToBase64String(_aes.Key), Convert.ToBase64String(_aes.IV));
+    return new AESKey(_aes.Key.ToBase64(), _aes.IV.ToBase64());
+  }
+
+  public async Task EncryptFileAsync(
+    string inputFilePath, string outputFilePath, AESKey aesKey)
+  {
+    var bytes = await File.ReadAllBytesAsync(inputFilePath);
+    var keyBytes = aesKey.Key.Base64ToBytes();
+    var ivBytes = aesKey.IV.Base64ToBytes();
+
+    using var encryptor = _aes.CreateEncryptor(keyBytes, ivBytes);
+    var encryptedBytes = encryptor.TransformFinalBlock(bytes, 0, bytes.Length);
+
+    await File.WriteAllBytesAsync(outputFilePath, encryptedBytes);
+  }
+
+  public async Task DecryptFileAsync(
+    string inputFilePath, string outputFilePath, AESKey aesKey)
+  {
+    var bytes = await File.ReadAllBytesAsync(inputFilePath);
+    var keyBytes = aesKey.Key.Base64ToBytes();
+    var ivBytes = aesKey.IV.Base64ToBytes();
+
+    using var decryptor = _aes.CreateDecryptor(keyBytes, ivBytes);
+    var decryptedBytes = decryptor.TransformFinalBlock(bytes, 0, bytes.Length);
+
+    await File.WriteAllBytesAsync(outputFilePath, decryptedBytes);
   }
 }
