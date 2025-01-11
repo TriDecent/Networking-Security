83c19fce78d30b1fce0619043bb2874d482a4fef
TriDecent <116976929+TriDecent@users.noreply.github.com>
2025-01-12 04:06:14 +0700

    add HeaderMetadataHandler class for managing HybridMetadataHeader operations

---
+++ DataEncryptionApp/CryptographicApp/CryptographicCores/MetadataHeaderExtractor/HeaderMetadataHandler.cs
@@ -0,0 +1,106 @@
+using System.Buffers.Binary;
+using CryptographicApp.CryptographicCores.Asymmetric;
+using CryptographicApp.CryptographicCores.HashGenerators;
+using CryptographicApp.Enums;
+using CryptographicApp.Models;
+using CryptographicApp.Utils;
+
+namespace CryptographicApp.CryptographicCores.MetadataHeaderExtractor;
+
+public class HeaderMetadataHandler(
+  IHashGenerator hashGenerator, IRSAEncryption rsa) : IHeaderMetadataHandler
+{
+  private const int LENGTH_PREFIX_SIZE = sizeof(int);
+  private readonly IHashGenerator _hashGenerator = hashGenerator;
+  private readonly IRSAEncryption _rsa = rsa;
+
+  public HybridMetadataHeader GenerateHeaderMetadata(
+    string filePath, RSAKey rsaKey, AESKey aesKey)
+  {
+    var inputFileHash = _hashGenerator.GenerateHash(filePath, DataFormat.File);
+
+    var encryptedAESKey = _rsa
+      .Encrypt(aesKey.Key, rsaKey.PublicKey, DataFormat.Text);
+
+    var encryptedAesIV = _rsa
+      .Encrypt(aesKey.IV, rsaKey.PublicKey, DataFormat.Text);
+
+    var encryptedHashBytes = _rsa
+      .Encrypt(inputFileHash, rsaKey.PublicKey, DataFormat.Text)
+      .HexToBytes();
+
+    var encryptedAESKeyBytes = encryptedAESKey.Base64ToBytes();
+    var encryptedAesIVBytes = encryptedAesIV.Base64ToBytes();
+
+    return new HybridMetadataHeader(
+      AESKeyLength: encryptedAESKeyBytes.Length,
+      AesIVLength: encryptedAesIVBytes.Length,
+      HashLength: encryptedHashBytes.Length,
+      EncryptedAesKey: new AESKey(encryptedAESKey, encryptedAesIV),
+      EncryptedHash: encryptedHashBytes.ToHex()
+    );
+  }
+
+  public async Task<HybridMetadataHeader> GetHeaderMetadata(string filePath)
+  {
+    using var fileStream = File.OpenRead(filePath);
+
+    var buffer = new byte[LENGTH_PREFIX_SIZE];
+
+    await fileStream.ReadExactlyAsync(buffer.AsMemory());
+    var aesKeyLength = BinaryPrimitives.ReadInt32BigEndian(buffer);
+
+    await fileStream.ReadExactlyAsync(buffer.AsMemory());
+    var aesIVLength = BinaryPrimitives.ReadInt32BigEndian(buffer);
+
+    await fileStream.ReadExactlyAsync(buffer.AsMemory());
+    var hashLength = BinaryPrimitives.ReadInt32BigEndian(buffer);
+
+    var aesKeyBytes = new byte[aesKeyLength];
+    var aesIVBytes = new byte[aesIVLength];
+    var hashBytes = new byte[hashLength];
+
+    await fileStream.ReadExactlyAsync(aesKeyBytes.AsMemory());
+    await fileStream.ReadExactlyAsync(aesIVBytes.AsMemory());
+    await fileStream.ReadExactlyAsync(hashBytes.AsMemory());
+
+    return new HybridMetadataHeader(
+        AESKeyLength: aesKeyLength,
+        AesIVLength: aesIVLength,
+        HashLength: hashLength,
+        EncryptedAesKey: new AESKey(aesKeyBytes.ToBase64(), aesIVBytes.ToBase64()),
+        EncryptedHash: hashBytes.ToHex()
+    );
+  }
+
+  public void SkipHeader(FileStream inputFileStream, HybridMetadataHeader headerMetadata)
+  {
+    var totalHeaderSize = LENGTH_PREFIX_SIZE * 3
+      + headerMetadata.AESKeyLength
+      + headerMetadata.AesIVLength
+      + headerMetadata.HashLength;
+
+    inputFileStream.Seek(totalHeaderSize, SeekOrigin.Current);
+  }
+
+  public async Task WriteMetadataHeaderTo(
+    Stream targetStream, HybridMetadataHeader metadataHeader)
+  {
+    var buffer = new byte[4];
+
+    BinaryPrimitives.WriteInt32BigEndian(buffer, metadataHeader.AESKeyLength);
+    await targetStream.WriteAsync(buffer.AsMemory());
+
+    BinaryPrimitives.WriteInt32BigEndian(buffer, metadataHeader.AesIVLength);
+    await targetStream.WriteAsync(buffer.AsMemory());
+
+    BinaryPrimitives.WriteInt32BigEndian(buffer, metadataHeader.HashLength);
+    await targetStream.WriteAsync(buffer.AsMemory());
+
+    await targetStream.WriteAsync(metadataHeader.EncryptedAesKey.Key.Base64ToBytes().AsMemory());
+
+    await targetStream.WriteAsync(metadataHeader.EncryptedAesKey.IV.Base64ToBytes().AsMemory());
+
+    await targetStream.WriteAsync(metadataHeader.EncryptedHash.HexToBytes().AsMemory());
+  }
+}
